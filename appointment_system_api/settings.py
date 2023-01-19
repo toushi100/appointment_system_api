6c139442caff5b9f233bceb12958af153cf19b97
Ahmed Salah <toushi2009@gmail.com>
2023-01-19 23:33:00 +0200

    change settings for develop

--- appointment_system_api/settings.py
+++ appointment_system_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2=3mzxc*obh+pvcisa4$xf)+%c9lfsh1#-)7nhv82_4=ix2a7f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
