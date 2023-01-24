e7d283013638594c3f914797b1083497a015e25e
Ahmed Salah <toushi2009@gmail.com>
2023-01-24 13:29:26 +0200

    added new models

--- appointment_system_api/urls.py
+++ appointment_system_api/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('user/', include('user.urls')),
+    path('user/', include('user_app.urls')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
