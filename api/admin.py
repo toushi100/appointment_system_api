462dc4f4dab2c95b1c6e97cb1ebbcb621e70c780
Ahmed Salah <toushi2009@gmail.com>
2023-02-04 01:52:45 +0200

    removed doctor model

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,11 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Doctor)
-admin.site.register(Clinic)
-admin.site.register(CareRoom)
-admin.site.register(Nursery)
-admin.site.register(Patient)
-admin.site.register(SurgicalOperation)
-admin.site.register(Schedule)
-admin.site.register(BloodBank)
-admin.site.register(CentralCare)
+# from django.contrib import admin
+# from .models import *
+# # Register your models here.
+# admin.site.register(Clinic)
+# admin.site.register(CareRoom)
+# admin.site.register(Nursery)
+# admin.site.register(Patient)
+# admin.site.register(SurgicalOperation)
+# admin.site.register(Schedule)
+# admin.site.register(BloodBank)
+# admin.site.register(CentralCare)
