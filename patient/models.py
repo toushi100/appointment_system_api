64630b8036354546697359c07690c000722e63a6
Ahmed Salah <toushi2009@gmail.com>
2023-02-10 00:12:58 +0200

    patient migrations

--- patient/models.py
+++ patient/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50, unique=True, null=True)
+    date_of_birth = models.DateField(null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient')
