9f5d62083748db9aa86ab8a53514a4476722d275
Ahmed Salah <toushi2009@gmail.com>
2023-02-10 00:16:41 +0200

    added appiontment to schedule serializer

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -1,22 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework  import serializers
 from rest_framework import serializers
-from .models import Schedule
+from .models import Schedule, Appointment
 from . import models
 from doctor.serializers import DoctorSerializer
 
 
-class AppointmentSerializer(ModelSerializer):
+class AppointmentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Schedule
+        model = Appointment
         fields = '__all__'
-class ScheduleSerializer(ModelSerializer):
+
+class ScheduleSerializer(serializers.ModelSerializer):
     days  = serializers.MultipleChoiceField(choices=models.DAYS)
     class Meta:
         model = Schedule
         fields = '__all__'
         
-class ShowScheduleSerializer(ModelSerializer):
+class ShowScheduleSerializer(serializers.ModelSerializer):
+
+    days = serializers.MultipleChoiceField(choices=models.DAYS)
     doctor = DoctorSerializer()
+    appointments = serializers.SerializerMethodField()
     class Meta:
         model = Schedule
         fields = '__all__'
+    
+    def get_appointments(self, obj):
+        appointments = obj.appointment_set.all()
+        serializer = AppointmentSerializer(appointments, many=True)
+        return serializer.data
