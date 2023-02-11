7c1f070fa519df4ca96cde1b747e0a72cfcca7df
Ahmed Salah <toushi2009@gmail.com>
2023-02-11 22:20:44 +0200

    added list_available list_reserved endpoints

--- schedule/urls.py
+++ schedule/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('create/', create, name='create'),
     path('show/<int:pk>/', show, name='show'),
     path('reserve/<int:pk>/', reserve, name='reserve'),
+    path('list_available/<int:doctor_id>/', list_available, name='list_available'),
+    path('list_reserved/', list_reserved, name='list_reserved'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -3,8 +3,11 @@ from rest_framework.decorators import api_view
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response 
-from .models import Schedule
+from .models import Schedule, Appointment
 from .serializers import *
+from datetime import timedelta
+from django.utils import timezone
+
 
 @api_view(['post'])
 @login_required
@@ -35,3 +38,21 @@ def reserve(request, pk):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['get'])
+def list_available(request,doctor_id):
+    appointments = Appointment.objects.filter(patient__isnull=True, start_time__gte=timezone.now(),
+                                              schedule__doctor__id=doctor_id)
+    serializer = ShowAppointmentSerializer(appointments, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['get'])
+@login_required
+def list_reserved(request):
+    doctor_id = request.user.doctor.id
+    appointments = Appointment.objects.filter(patient__isnull=False,schedule__doctor__id=doctor_id,
+                                              start_time__range=[timezone.now()-timedelta(days = 7),
+                                                                   timezone.now()+timedelta(days = 7)]
+)
+    serializer = ShowAppointmentSerializer(appointments, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
