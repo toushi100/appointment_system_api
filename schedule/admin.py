9f613bd26d9402f19170017e3ef395f25f0b868b
Ahmed Salah <toushi2009@gmail.com>
2023-02-12 21:24:35 +0200

    format

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Schedule, Appointment
-# Register your models here.
+
+
 class ScheduleAdmin(admin.ModelAdmin):
     list_display = ('id', 'doctor', 'days', 'start_time', 'end_time')
+
+
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = ('id', 'schedule', 'patient', 'start_time', 'end_time')
+
+
 admin.site.register(Schedule, ScheduleAdmin)
 admin.site.register(Appointment, AppointmentAdmin)
