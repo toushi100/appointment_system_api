2f94faf7f6b4d15bf54d9e6e56cd2f79fea74c59
Ahmed Salah <toushi2009@gmail.com>
2023-02-13 02:07:48 +0200

    added surgery to admin

--- surgery/admin.py
+++ surgery/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
-from .models import Surgery
+from .models import Surgery, SurgeryType
+
 
 class SurgeryAdmin(admin.ModelAdmin):
     list_display = ('room', 'start_time', 'end_time',  'patient')
     list_filter = ('room', 'start_time', 'end_time')
     search_fields = ('room', 'start_time', 'end_time')
-    ordering = ('room', 'start_time', 'end_time') 
-    
-admin.site.register(Surgery, SurgeryAdmin) 
+    ordering = ('room', 'start_time', 'end_time')
+
+
+class SurgeryTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+admin.site.register(Surgery, SurgeryAdmin)
+admin.site.register(SurgeryType, SurgeryTypeAdmin)
