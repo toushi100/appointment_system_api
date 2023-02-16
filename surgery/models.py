e3e3b75ac89b746dcbabb9e18faadfd605c3fec7
Ahmed Salah <toushi2009@gmail.com>
2023-02-16 23:48:24 +0200

    added surgery model

--- surgery/models.py
+++ surgery/models.py
@@ -4,29 +4,37 @@ from patient.models import Patient
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+from datetime import timedelta
 ROOM_CHOICES = (
     ('OR_1', 'Operation Room 1'),
     ('OR_2', 'Operation Room 2'),
     ('OR_3', 'Operation Room 3'),
 )
 
-
+surgery_type_choices = (
+    ('1', 'Heart Surgery'),
+    ('2', 'Brain Surgery'),
+    ('3', 'Cancer Surgery'),
+    ('4', 'Lung Surgery'),
+    ('5', 'Other'),
+)
 def validate_start_time_end_time(value):
     if value < timezone.now():
         raise ValidationError(
-            _('%(value)s is not a valid start time'),
+             _('%(value)s is not a valid start time'),
             params={'value': value},
-        )
-
-
+            )
+    
 class Surgery(models.Model):
     doctors = models.ManyToManyField(Doctor)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     room = models.CharField(max_length=100, choices=ROOM_CHOICES)
     start_time = models.DateTimeField(
         validators=[validate_start_time_end_time])
-    end_time = models.DateTimeField(validators=[validate_start_time_end_time])
-    surgery_type = models.ForeignKey('SurgeryType', on_delete=models.CASCADE)
+    end_time = models.DateTimeField(validators=[validate_start_time_end_time],default=None, blank=True, null=True)
+    estimated_time = models.DurationField(default=timedelta(minutes=30))
+    surgery_type = models.CharField(max_length=100, choices=surgery_type_choices, default='5')
+    
 
     class Meta:
         verbose_name = 'Surgery'
@@ -43,12 +51,3 @@ class Surgery(models.Model):
             raise ValidationError("Surgery overlaps with another surgery.")
 
 
-class SurgeryType(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Surgery Type'
-        verbose_name_plural = 'Surgery Types'
