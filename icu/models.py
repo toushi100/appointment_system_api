6df5a861549d6212f05db51773d0739b1e1580db
Ahmed Salah <toushi2009@gmail.com>
2023-02-18 01:23:03 +0200

    added intensive careunit model

--- icu/models.py
+++ icu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from  patient.models import Patient
 
-# Create your models here.
+bed_choices = (('bed_1','Bed 1'),
+               ('bed_2','Bed 2'),
+               ('bed_3','Bed 3'),
+               ('bed_4','Bed 4'),
+               ('bed_5','Bed 5'),)
+
+class IntensiveCareUnit(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    bed = models.CharField(max_length=100, choices=bed_choices)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    estimated_time = models.DateTimeField()
+   
+    def __str__(self):
+        return f'{self.patient} {self.bed} {self.start_time} {self.end_time} {self.estimated_time}'
