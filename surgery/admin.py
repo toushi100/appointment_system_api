a4141b577cd07716135b8b444e018cd874a6468c
Ahmed Salah <toushi2009@gmail.com>
2023-02-18 00:33:39 +0200

    added id to admin

--- surgery/admin.py
+++ surgery/admin.py
@@ -3,10 +3,10 @@ from .models import Surgery
 
 
 class SurgeryAdmin(admin.ModelAdmin):
-    list_display = ('room', 'start_time', 'end_time',  'patient')
+    list_display = ('id','room', 'start_time', 'end_time',  'patient')
     list_filter = ('room', 'start_time', 'end_time')
     search_fields = ('room', 'start_time', 'end_time')
-    ordering = ('room', 'start_time', 'end_time')
+    ordering = ('id','room', 'start_time', 'end_time')
 
 
 admin.site.register(Surgery, SurgeryAdmin)
