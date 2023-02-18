57f95333afda68195160f8e6ada139982db8044d
Ahmed Salah <toushi2009@gmail.com>
2023-02-18 02:42:21 +0200

    added create endpoint

--- icu/views.py
+++ icu/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from .models import IntensiveCareUnit
+from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['POST'])
+def create(request):
+    serializers = IntensiveCareUnitSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
