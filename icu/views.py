e477f48480ce8b26a5f7aad77d5369c9520bfad3
Ahmed Salah <toushi2009@gmail.com>
2023-02-22 19:39:52 +0200

    fixed typo in file name

--- icu/views.py
+++ icu/views.py
@@ -4,7 +4,7 @@ from .serializers import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.utils import timezone
-from .fiters import IntensiveCareUnitFilter
+from .filters import IntensiveCareUnitFilter
 
 
 @api_view(['POST'])
