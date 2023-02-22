ac928275cf4aeeba191320f584236ec0bb733181
Ahmed Salah <toushi2009@gmail.com>
2023-02-22 21:26:45 +0200

    added serializers for bloodbank model

--- bloodbank/serializers.py
+++ bloodbank/serializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializers
+from .models import BloodBank
+
+class ShowBloodBankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BloodBank
+        fields = '__all__'
+        depth = 1
+
+class BloodBankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BloodBank
+        fields = '__all__'
