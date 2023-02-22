873278ce116543996af824f46961798fccecb3f8
Ahmed Salah <toushi2009@gmail.com>
2023-02-22 19:41:43 +0200

    added  create reserved_incubators update endpoints

--- incubator/urls.py
+++ incubator/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('create/', create, name='create'),
+    path('reserved_incubators/', reserved_incubators, name='reserved_incubators'),
+    path('update/<int:pk>/', update, name='update'),
 ]
--- incubator/views.py
+++ incubator/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Incubator
+from .serializers import *
+from .filters import IncubatorFilter
+from django.utils import timezone
 
-# Create your views here.
+
+@api_view(['POST'])
+def create(request):
+    serializers = IncubatorSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def reserved_incubators(request):
+    
+    filters = request.data.get('filters', {})
+    icu = Incubator.objects.filter(start_date__lte=timezone.now(), end_date= None)
+    icu = IncubatorFilter(filters, queryset=icu).qs
+    serializers = ShowIncubatorSerializer(icu, many=True)
+    return Response(serializers.data)
+
+@api_view(['PUT', 'PATCH'])
+def update(request, pk):
+    icu = Incubator.objects.get(id=pk)
+    serializers = IncubatorSerializer(instance=icu, data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_202_ACCEPTED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    
