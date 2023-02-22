a34236f72e73b4a00cf04f3333ae3bbaad32f90e
Ahmed Salah <toushi2009@gmail.com>
2023-02-22 19:40:43 +0200

    added filters to incubator

--- incubator/filters.py
+++ incubator/filters.py
@@ -1,1 +1,11 @@
 import django_filters
+from .models import Incubator
+
+class IncubatorFilter(django_filters.FilterSet):
+    patient = django_filters.NumberFilter(field_name='patient', lookup_expr='exact')
+    incubator = django_filters.CharFilter(field_name='incubator', lookup_expr='exact')
+    start_date__gte = django_filters.DateFilter(field_name='start_date', lookup_expr='gte')
+    
+    class Meta:
+        model = Incubator
+        fields ='__all__'
