e9ee0164ac722bfb9245f9c18688e985d1cc4b56
Ahmed Salah <toushi2009@gmail.com>
2023-02-22 19:40:58 +0200

    added incubator serializers

--- incubator/serializers.py
+++ incubator/serializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializers
+from .models import Incubator
+
+class ShowIncubatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Incubator
+        fields = '__all__'
+        depth = 1
+        
+class IncubatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Incubator
+        fields = '__all__'
