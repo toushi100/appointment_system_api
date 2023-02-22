38802b874a23b022d23f17b8396d23f65393390b
Ahmed Salah <toushi2009@gmail.com>
2023-02-22 21:28:59 +0200

    added create and index endpoints

--- bloodbank/urls.py
+++ bloodbank/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('index/', index, name='index'),
+    path('create/', create, name='create'),
+    
 ]
--- bloodbank/views.py
+++ bloodbank/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import BloodBank
+from .filters import *
+from .serializers import *
 
-# Create your views here.
+@api_view(['GET'])
+def index(request):
+    filters = request.data.get('filters', {})
+    donations = BloodBank.objects.all()
+    donations = BloodBankFilter(request.GET, queryset=donations).qs
+    serializers = ShowBloodBankSerializer(donations, many=True)
+    return Response(serializers.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def create(request):
+    serializers = BloodBankSerializer(data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
