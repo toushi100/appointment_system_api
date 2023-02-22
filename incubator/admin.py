3627fe999f4f4d0b7b53eb584a62dae305497da4
Ahmed Salah <toushi2009@gmail.com>
2023-02-22 19:40:18 +0200

    added incubator to admin

--- incubator/admin.py
+++ incubator/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Incubator
 
-# Register your models here.
+class IncubatorAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'start_date', 'end_date', 'incubator')
+    list_filter = ('incubator', 'start_date', 'end_date')
+    search_fields = ('patient__first_name', 'patient__last_name', 'patient__id')
+    
+admin.site.register(Incubator, IncubatorAdmin)
