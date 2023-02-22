eee0cc67b33c81d3b52d77896e7023ddfba73878
Ahmed Salah <toushi2009@gmail.com>
2023-02-22 21:26:27 +0200

    added filters for bloodbank

--- bloodbank/filters.py
+++ bloodbank/filters.py
@@ -1,1 +1,8 @@
 import django_filters
+from .models import BloodBank
+
+class BloodBankFilter(django_filters.FilterSet):
+    blood_type = django_filters.CharFilter(field_name="blood_type" ,lookup_expr='exact')
+    class Meta:
+        model = BloodBank
+        fields = ['blood_type']
