dae275be751ec6e6cea4d7402395f676f3c19406
Ahmed Salah <toushi2009@gmail.com>
2023-04-28 14:09:16 +0300

    added patient and doctor to user serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
 from . models import User 
+from patient.serializers import PatientSerializer
+from doctor.serializers import DoctorSerializer
 
 class UserSerializer(serializers.ModelSerializer):
 
+    patient = serializers.SerializerMethodField()
+    doctor = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('id','email', 'user_name', 'password')
+        fields = '__all__'
+        
         extra_kwargs = {'password': {'write_only': True}}
 
+    def get_patient(self, obj):
+        patient = obj.patient
+        serializer = PatientSerializer(patient)
+        return serializer.data 
+    
+    def get_doctor(self, obj):
+        patient = obj.doctor
+        serializer = DoctorSerializer(patient)
+        return serializer.data 
+ 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
         # as long as the fields are the same, we can just use this
