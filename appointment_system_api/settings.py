1a070f4635b1455e576ce1d5b7000fd7aa7b3134
Ahmed Salah <toushi2009@gmail.com>
2023-05-24 18:12:03 +0300

    deleting migrations and creating a new one

--- appointment_system_api/settings.py
+++ appointment_system_api/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'appointment_system_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appointment_system_development',
+        'USER': 'ahmed',
+        'PASSWORD': 'Adham85m',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
