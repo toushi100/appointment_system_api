e57e0103f9eea21008aff176d6070372db2add02
Ahmed Salah <toushi2009@gmail.com>
2023-05-26 12:02:29 +0300

    fix bloodbank viewname

--- bloodbank/views.py
+++ bloodbank/views.py
@@ -5,12 +5,13 @@ from .models import BloodBank, BloodbankRequest
 from rest_framework.reverse import reverse
 from .filters import *
 from .serializers import *
-
+import requests
 @api_view(['GET'])
 def   total_amount(request, blood_type):
     amount = BloodBank.objects.total_amount(blood_type)
     response={
-        blood_type: f'{amount["amount__sum"]} bag(s)'
+        "blood_type": blood_type,
+        "amount_in_bags": amount['amount__sum']
     }
     return Response(response, status=status.HTTP_200_OK)
 
@@ -24,8 +25,10 @@ def create(request):
 
 @api_view(['post'])
 def create_bloodbank_request(request):
-    amount = reverse(name= 'total_amount', request=request, args=[request.data['blood_type']])
-    if request.data['amount'] > amount:
+
+    amount = reverse(viewname= 'total_amount', request=request, args=[request.data['blood_type']])
+    amount = requests.get(amount).json()
+    if request.data['amount'] > amount.get('amount_in_bags'):
         return Response({'error': 'amount is greater than available amount'}, status=status.HTTP_400_BAD_REQUEST)
     serializers = BloodBankRequestSerializer(data=request.data)
     if serializers.is_valid():
